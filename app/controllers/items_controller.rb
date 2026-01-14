098803b47d9ff1512ef77f4b3addedd8392a9f78
Obaid-Devsinc <obaid.ali@devsinc.com>
2026-01-14 18:12:45 +0500

    feat: add category filter dropdown on menu page

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,10 +1,16 @@
 class ItemsController < ApplicationController
   def index
-    @items = Item.active.select(:id, :title, :price, :slug)
+    @categories = Category.all.select(:id, :name)
+
+    @items = Item.active.select(:id, :title, :price, :slug, :category_id)
 
     if params[:query].present?
       query = params[:query].downcase
       @items = @items.where("LOWER(title) LIKE ?", "%#{query}%")
+    end
+
+    if params[:category].present?
+      @items = @items.where(category_id: params[:category])
     end
 
     @items = @items.page(params[:page]).per(9)
@@ -12,8 +18,8 @@ class ItemsController < ApplicationController
 
   def show
     @item = Item
-    .includes(:category)
-    .select(:id, :title, :price, :slug, :description, :category_id)
-    .find_by(slug: params[:id])
+      .includes(:category)
+      .select(:id, :title, :price, :slug, :description, :category_id)
+      .find_by(slug: params[:id])
   end
 end
